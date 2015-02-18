bd70211a7bbb9ee22af8a1ebf663e2161495721d
Adam <ajmichael0@gmail.com>
2015-02-17 23:08:16 -0500

    getting response from tracker, but its an error response

---
+++ src/main/java/AnotherTutorial.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by michaea1 on 2/17/2015.
+ */
+class AnotherTutorial {
+
+}
--- src/main/java/Driver.scala
+++ src/main/java/Driver.scala
@@ -1,5 +1,13 @@
 import java.io.File
+import java.net.{URLConnection, URL}
 import java.security.MessageDigest
+
+import akka.io.IO
+import akka.remote.transport.netty.NettyTransportSettings.Udp
+import spray.client.pipelining._
+import spray.http.HttpRequest
+import sun.net.www.content.text.PlainTextInputStream
+import org.apache.commons.io.IOUtils
 
 import scala.io.Codec.ISO8859
 import scala.io.Source
@@ -35,14 +43,36 @@ object Driver {
     var encoded_peer_id = MessageDigest.getInstance("SHA-1").digest(peer_id).toString
     encoded_peer_id = java.net.URLEncoder.encode(encoded_peer_id)
     println("Peer_id: "+encoded_peer_id)
-    var port = 8080
+    var event="started"
+    var port = 6881
     var uploaded = "0"
     var downloaded = "0"
     var left = metainfo.fileLengths.values.sum
-    var compact = "0"
-    var trackerURL = metainfo.announce+"?info_hash="+encodedInfoHash+"&peer_id="+peer_id+"&port="+port.toString+"&uploaded="+uploaded+"&downloaded="+downloaded+"&left="+left+"&compact="+compact
-    println(trackerURL)
-    
+    var compact = "1"
+    var trackerURL = metainfo.announce+"?event="+event+"&info_hash="+encodedInfoHash+"&peer_id="+encoded_peer_id+"&port="+port.toString+"&uploaded="+uploaded+"&downloaded="+downloaded+"&left="+left+"&compact="+compact+"&no_peer_id=0"
+    val conn: URLConnection = new URL(trackerURL.replace("udp","http")).openConnection
+    val what: AnyRef = conn.getContent
+    println(conn)
+    println(what)
+    what match {
+      case text: PlainTextInputStream => {
+        var content = IOUtils.toString(text,"UTF-8")
+        println(content)
+      }
+    }
+    var res: HttpRequest = Get(trackerURL)
+    IO(Udp) ! Udp.SimpleSenderReady =>
+      context.become(ready(sender()))
+
+    //new Tracker(metainfo,trackerURL).sendRequest
+
+    //implicit val system = ActorSystem("simple-spray-client")
+    //val log = Logging(system, getClass)
+    //log.info("Requesting stuff")
+    //println(trackerURL)
+
+
+
     //val result = Decode(chars)
     //println(result)
   }
